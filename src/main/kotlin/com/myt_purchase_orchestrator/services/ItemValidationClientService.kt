62c913eac63daa3805f43df330ca9cd661918a7f
natanPrs <natan.void@proton.me>
2025-05-30 11:02:02 -0300

    feat: item feign config

---
+++ src/main/kotlin/com/myt_purchase_orchestrator/services/ItemValidationClientService.kt
@@ -0,0 +1,14 @@
+package com.myt_purchase_orchestrator.services
+
+import com.myt_purchase_orchestrator.dtos.PurchaseDto
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import java.math.BigDecimal
+
+@FeignClient(name = "item-validation-service", url = "http://localhost:8082/catalog")
+interface ItemValidationClientService {
+
+    @PostMapping("/purchase-validate-item")
+    fun sendItemToValidate(@RequestBody purchaseDto: PurchaseDto): BigDecimal?
+}
