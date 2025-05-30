a4209bc1f3c54167016720e633005664fa4f5757
natanPrs <natan.void@proton.me>
2025-05-30 11:00:39 -0300

    feat: kafka producer config

---
+++ src/main/kotlin/com/myt_purchase_orchestrator/configs/KafkaProducerConfig.kt
@@ -0,0 +1,29 @@
+package com.myt_purchase_orchestrator.configs
+
+import com.myt_purchase_orchestrator.dtos.SavePurchaseDto
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+@Configuration
+class KafkaProducerConfig(
+    @Value("\${spring.kafka.bootstrap-servers}")
+    val bootstrapServer: String) {
+
+    fun producerFactory(): ProducerFactory<String, SavePurchaseDto>{
+        val config = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServer,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java
+        )
+        return DefaultKafkaProducerFactory(config)
+    }
+
+    fun templateKafka(): KafkaTemplate<String, SavePurchaseDto> =
+        KafkaTemplate(producerFactory())
+}
