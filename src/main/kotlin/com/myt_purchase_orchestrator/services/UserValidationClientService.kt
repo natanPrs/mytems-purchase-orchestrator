68f74bb0ca0507ea0fe56f449590b216c2b71f75
natanPrs <natan.void@proton.me>
2025-06-18 10:17:43 -0300

    feat: changing return from Boolean to UUID

--- src/main/kotlin/com/myt_purchase_orchestrator/services/UserValidationClientService.kt
+++ src/main/kotlin/com/myt_purchase_orchestrator/services/UserValidationClientService.kt
@@ -4,11 +4,12 @@ import com.myt_purchase_orchestrator.dtos.PurchaseDto
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import java.util.UUID
 
 
 @FeignClient(name = "user-validation-service", url = "http://localhost:8081/validate-users")
 interface UserValidationClientService {
 
     @PostMapping("/purchase-validate-user")
-    fun sendUsersToValidate(@RequestBody purchaseDto: PurchaseDto): Boolean
+    fun sendUsersToValidate(@RequestBody purchaseDto: PurchaseDto): UUID?
 }
