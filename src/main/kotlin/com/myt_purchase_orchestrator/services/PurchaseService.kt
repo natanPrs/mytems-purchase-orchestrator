64333f3a3d6a9033cda8dffca6b673e1f8227105
natanPrs <natan.void@proton.me>
2025-06-23 23:12:43 -0300

    feat: add trace logs

--- src/main/kotlin/com/myt_purchase_orchestrator/services/PurchaseService.kt
+++ src/main/kotlin/com/myt_purchase_orchestrator/services/PurchaseService.kt
@@ -5,27 +5,44 @@ import com.myt_purchase_orchestrator.dtos.EventSavePurchaseDto
 import com.myt_purchase_orchestrator.dtos.EventUpdateItemStatusDto
 import com.myt_purchase_orchestrator.dtos.EventUpdateUserDto
 import com.myt_purchase_orchestrator.mappers.toEventUpdateStatus
+import com.myt_purchase_orchestrator.producers.PurchaseKafkaProducer
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.util.UUID
+import kotlin.math.log
 
 @Service
 class PurchaseService(
     private val userValidationClientService: UserValidationClientService,
     private val itemValidationClientService: ItemValidationClientService,
-    private val eventUpdateItemStatusDtoTemplate: KafkaTemplate<String, EventUpdateItemStatusDto>,
-    private val eventUpdateUserTemplate: KafkaTemplate<String, EventUpdateUserDto>,
-    private val eventSavePurchaseTemplate: KafkaTemplate<String, EventSavePurchaseDto>) {
+    private val purchaseKafkaProducer: PurchaseKafkaProducer){
+
+    private val logger = LoggerFactory.getLogger(PurchaseService::class.java)
 
     fun realizePurchase(purchaseDto: PurchaseDto){
-        val validateUserAndGetSellerId = validateUser(purchaseDto)
-        val validateItemAndGetAmount = validateItem(purchaseDto)
+        try {
+            logger.info("Processing purchase for buyerId={} sellerId{}, globalItemId{}",
+                purchaseDto.buyerId, purchaseDto.sellerId, purchaseDto.globalItemId)
 
-        sendSavePurchaseEvent(purchaseDto, validateUserAndGetSellerId, validateItemAndGetAmount)
-        sendItemStatusEvent(purchaseDto)
-        sendUserUpdateEvent(validateItemAndGetAmount, purchaseDto.buyerId,purchaseDto.globalItemId)
+            val validateUserAndGetSellerId = validateUser(purchaseDto)
+            val validateItemAndGetAmount = validateItem(purchaseDto)
+
+            purchaseKafkaProducer.sendSavePurchaseEvent(purchaseDto, validateUserAndGetSellerId, validateItemAndGetAmount)
+            purchaseKafkaProducer.sendItemStatusEvent(purchaseDto)
+            purchaseKafkaProducer.sendUserUpdateEvent(validateItemAndGetAmount, purchaseDto.buyerId,purchaseDto.globalItemId)
+
+            logger.info("Purchase successfully completed for item {}", purchaseDto.globalItemId)
+        } catch (ex: IllegalStateException){
+            logger.error("Validate error ex{}", ex.message)
+            throw ex
+        } catch (ex: Exception){
+            logger.error("Unexpected error during purchase processing ex{}", ex.message)
+            throw RuntimeException("Failure during purchase processing, try again.")
+        }
+
     }
 
     private fun validateUser(purchaseDto: PurchaseDto): UUID =
@@ -34,24 +51,7 @@ class PurchaseService(
     private fun validateItem(purchaseDto: PurchaseDto): BigDecimal =
         itemValidationClientService.sendItemToValidate(purchaseDto) ?: throw Exception("The Item return was Null.")
 
-    private fun sendSavePurchaseEvent(purchaseDto: PurchaseDto, sellerId: UUID, amount: BigDecimal){
-        val event = EventSavePurchaseDto(
-            globalItemId = purchaseDto.globalItemId,
-            sellerId = sellerId,
-            buyerId = purchaseDto.buyerId,
-            amount = amount,
-            localStamp = LocalDateTime.now())
 
-        eventSavePurchaseTemplate.send("savePurchase", event)
-    }
 
-    private fun sendItemStatusEvent(purchaseDto: PurchaseDto){
-        val event = purchaseDto.toEventUpdateStatus()
-        eventUpdateItemStatusDtoTemplate.send("updateItemStatus", event)
-    }
 
-    private fun sendUserUpdateEvent(amount: BigDecimal, buyerId : UUID, itemId: UUID){
-        val event = EventUpdateUserDto(amount, buyerId ,itemId)
-        eventUpdateUserTemplate.send("updateUser", event)
-    }
 }
