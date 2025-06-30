202854ff9a31643a08c9dbd7e5566f7507aa4b94
natanPrs <natan.void@proton.me>
2025-06-30 01:13:16 -0300

    feat: add outbox repo

--- src/main/kotlin/com/myt_purchase_orchestrator/outbox/repository/OutboxRepository.kt
+++ src/main/kotlin/com/myt_purchase_orchestrator/outbox/repository/OutboxRepository.kt
@@ -1,6 +1,7 @@
-package com.myt_purchase_orchestrator.outbox
+package com.myt_purchase_orchestrator.outbox.repository
 
 import com.myt_purchase_orchestrator.enums.OutboxEventStatus
+import com.myt_purchase_orchestrator.outbox.model.OutboxEventModel
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
