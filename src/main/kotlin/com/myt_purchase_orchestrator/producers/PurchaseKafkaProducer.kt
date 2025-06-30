c2d612cb5f75a74e4c5beed6d6aa44f7e8b53ca9
natanPrs <natan.void@proton.me>
2025-06-30 01:15:12 -0300

    feat: adding outbox

--- src/main/kotlin/com/myt_purchase_orchestrator/producers/PurchaseKafkaProducer.kt
+++ src/main/kotlin/com/myt_purchase_orchestrator/producers/PurchaseKafkaProducer.kt
@@ -8,10 +8,6 @@ import com.myt_purchase_orchestrator.mappers.toEventUpdateStatus
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
-import java.math.BigDecimal
-import java.time.LocalDateTime
-import java.util.UUID
-import kotlin.math.log
 
 @Component
 class PurchaseKafkaProducer(
@@ -22,26 +18,18 @@ class PurchaseKafkaProducer(
 
     private val logger = LoggerFactory.getLogger(PurchaseKafkaProducer::class.java)
 
-    fun sendSavePurchaseEvent(purchaseDto: PurchaseDto, sellerId: UUID, amount: BigDecimal){
-        val event = EventSavePurchaseDto(
-            globalItemId = purchaseDto.globalItemId,
-            sellerId = sellerId,
-            buyerId = purchaseDto.buyerId,
-            amount = amount,
-            localStamp = LocalDateTime.now())
-
+    fun sendSavePurchaseEvent(event: EventSavePurchaseDto){
         eventSavePurchaseTemplate.send("savePurchase", event)
         logger.debug("Kafka event sent.")
     }
 
-    fun sendItemStatusEvent(purchaseDto: PurchaseDto){
-        val event = purchaseDto.toEventUpdateStatus()
+    fun sendItemStatusEvent(event: PurchaseDto){
+        val event = event.toEventUpdateStatus()
         eventUpdateItemStatusDtoTemplate.send("updateItemStatus", event)
         logger.debug("Kafka event sent")
     }
 
-    fun sendUserUpdateEvent(amount: BigDecimal, buyerId : UUID, itemId: UUID){
-        val event = EventUpdateUserDto(amount, buyerId ,itemId)
+    fun sendUserUpdateEvent(event: EventUpdateUserDto){
         eventUpdateUserTemplate.send("updateUser", event)
     }
 }
